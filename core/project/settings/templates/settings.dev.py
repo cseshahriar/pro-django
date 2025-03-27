c99b8aae355557663c18137794c866cd213b1a2d
cseshahriar <cse.shahriar.hosen@gmail.com>
2025-03-27 07:27:06 +0600

    chore: logging config

--- core/project/settings/templates/settings.dev.py
+++ core/project/settings/templates/settings.dev.py
@@ -1,2 +1,10 @@
 SECRET_KEY = 'django-insecure-6lkd-cvq$gu3r&%^atqhy-o_rmrm(1afh(zk2-a86w*r2_vdwd'
 DEBUG = True
+
+LOGGING['formatters']['colored'] = {  # type: ignore  # noqa
+    '()': 'colorlog.ColoredFormatter',
+    'format': '%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s',
+}
+LOGGING['loggers']['cooking_core']['level'] = 'DEBUG'  # type: ignore # noqa
+LOGGING['handlers']['console']['level'] = 'DEBUG'  # type: ignore # noqa
+LOGGING['handlers']['console']['formatter'] = 'colored'  # type: ignore # noqa
--- core/project/urls.py
+++ core/project/urls.py
@@ -1,5 +1,13 @@
+import logging
+
 from django.contrib import admin
 from django.urls import path
+
+logger = logging.getLogger(__name__)
+logger.debug('------------ Project urls.py loaded')
+logger.info('------------ Project urls.py loaded')
+logger.warning('------------ Project urls.py loaded')
+logger.error('------------ Project urls.py loaded')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
